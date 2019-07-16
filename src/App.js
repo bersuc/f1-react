6e97ae1e5f34f0d6ceea6d3c8d4bacaa5f3caca7
Diogo Bersuc <dbersuc@gmail.com>
2019-07-16 20:55:24 -0300

    Criando tela de 404 e testando yarn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import Pilotos from './components/Pilotos';
 import Tabela from './components/Tabela';
-import DriverDetail from './components/DriverDetail'
+import DriverDetail from './components/DriverDetail';
+import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/tabela" exact component={Tabela} />
         <Route path="/pilotos" exact component={Pilotos} />
         <Route path="/pilotos/:id" exact component={DriverDetail} />
+        <Route path="/*" component={NotFound} />
       </Switch>
       
     </div>
---
+++ src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/NotFound.css';
+
+// componentDidMount() {
+//     document.title = 'Your page title here';
+//   };
+
+function NotFound() {
+
+    
+  
+  return (
+    <div className="notfound">
+      <p>404 - Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
